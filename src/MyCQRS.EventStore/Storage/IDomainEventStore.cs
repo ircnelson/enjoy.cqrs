d1fdd0ec6f9bb3e6a3e87d6dead3d7b6461a62c9
Nelson Junior <ircnelson@gmail.com>
2016-03-30 13:07:47 -0400

    IDomainEventStore doc

--- src/MyCQRS.EventStore/Storage/IDomainEventStore.cs
+++ src/MyCQRS.EventStore/Storage/IDomainEventStore.cs
@@ -1,16 +1,28 @@
-﻿using MyCQRS.Events;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyCQRS.Events;
 
 namespace MyCQRS.EventStore.Storage
 {
+    /// <summary>
+    /// Event Store repository abstraction.
+    /// </summary>
+    /// <typeparam name="TDomainEvent"></typeparam>
     public interface IDomainEventStore<TDomainEvent> : ITransactional
         where TDomainEvent : IDomainEvent
     {
+
+        /// <summary>
+        /// Retrieve all events based on <param name="aggregateId"></param>.
+        /// </summary>
+        /// <param name="aggregateId"></param>
+        /// <returns></returns>
         IEnumerable<TDomainEvent> GetAllEvents(Guid aggregateId);
+
+        /// <summary>
+        /// Save the aggregate events in Event Store.
+        /// </summary>
+        /// <param name="aggregate"></param>
         void Save(IAggregate aggregate);
     }
 }
