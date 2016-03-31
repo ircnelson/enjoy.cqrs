3ad6109a75d98d7c2c93229da4da73848c3e31ee
Nelson Junior <ircnelson@gmail.com>
2016-03-31 09:44:37 -0400

    aggregate root tests

---
+++ src/MyCQRS.TestFramework/AggregateRootTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using MyCQRS.Events;
+using MyCQRS.EventStore;
+
+namespace MyCQRS.TestFramework
+{
+    public abstract class AggregateRootTestFixture<TAggregateRoot> where TAggregateRoot : IAggregate, new()
+    {
+        protected TAggregateRoot AggregateRoot;
+        protected Exception CaughtException;
+        protected IEnumerable<IDomainEvent> PublishedEvents;
+        protected virtual IEnumerable<IDomainEvent> Given()
+        {
+            return new List<IDomainEvent>();
+        }
+        protected virtual void Finally() { }
+        protected abstract void When();
+        
+        protected AggregateRootTestFixture()
+        {   
+            CaughtException = new ThereWasNoExceptionButOneWasExpectedException();
+            AggregateRoot = new TAggregateRoot();
+            AggregateRoot.LoadFromHistory(Given());
+
+            try
+            {
+                When();
+                PublishedEvents = AggregateRoot.UncommitedEvents;
+            }
+            catch (Exception exception)
+            {
+                CaughtException = exception;
+            }
+            finally
+            {
+                Finally();
+            }
+        }
+    }
+}
---
+++ test/MyCQRS.UnitTests/Domain/AggregateTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using MyCQRS.Events;
+using MyCQRS.EventStore;
+using MyCQRS.TestFramework;
+using Xunit;
+
+namespace MyCQRS.UnitTests.Domain
+{
+    public class Registered_event_in_aggregate : AggregateRootTestFixture<TestAggregateRoot>
+    {
+        protected override void When()
+        {
+            AggregateRoot.DoSomething("Walter White");
+        }
+
+        [Fact]
+        public void Then_some_event_should_be_published()
+        {
+            PublishedEvents.Last().Should().BeAssignableTo<SomeEvent>();
+        }
+    }
+
+    public class Not_registered_event_in_aggregate : AggregateRootTestFixture<TestAggregateRoot>
+    {
+        protected override void When()
+        {
+            AggregateRoot.DoSomethingWithoutEvent();
+        }
+
+        [Fact]
+        public void Then_throws_an_exception()
+        {
+            CaughtException.Should().BeAssignableTo<ThereWasNoExceptionButOneWasExpectedException>();
+        }
+    }
+
+    public class TestAggregateRoot : Aggregate
+    {
+        public string Name { get; private set; }
+
+        public TestAggregateRoot()
+        {
+            On<SomeEvent>(x =>
+            {
+                Name = x.Name;
+            });
+        }
+        
+        public void DoSomething(string name)
+        {
+            Raise(new SomeEvent(name));
+        }
+
+        public void DoSomethingWithoutEvent()
+        {
+            Raise(new NotRegisteredEvent());
+        }
+    }
+
+    public class SomeEvent : DomainEvent
+    {
+        public string Name { get; }
+
+        public SomeEvent(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class NotRegisteredEvent : DomainEvent
+    {
+    }
+}
