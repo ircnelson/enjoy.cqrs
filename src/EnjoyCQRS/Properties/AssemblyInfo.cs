3ccda12fbeffe29720a409c7d06bbee4881dabab
Nelson Júnior <ircnelson@gmail.com>
2016-04-30 01:07:36 -0400

    close issue #4

--- src/EnjoyCQRS/Properties/AssemblyInfo.cs
+++ src/EnjoyCQRS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EnjoyCQRS.Events")]
+[assembly: AssemblyTitle("EnjoyCQRS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EnjoyCQRS.Events")]
+[assembly: AssemblyProduct("EnjoyCQRS")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5512af07-a3bb-4a41-a0b4-28c930c57145")]
+[assembly: Guid("7cedd8f7-210d-44fc-b690-9079af5a12df")]
 
 // Version information for an assembly consists of the following four values:
 //
