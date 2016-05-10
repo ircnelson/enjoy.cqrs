245b28070024d3c7a28e44f3e6c629f8ce9c7426
Nelson Junior <ircnelson@gmail.com>
2016-05-10 09:21:05 -0400

    Added exception when aggregate not found

---
+++ src/EnjoyCQRS/EventSource/Exceptions/AggregateNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EnjoyCQRS.EventSource.Exceptions
+{
+    public class AggregateNotFoundException : Exception
+    {
+        public string AggregateName { get; }
+        public Guid AggregateId { get; }
+
+        public AggregateNotFoundException(string aggregateName, Guid aggregateId) : base($"The aggregate '{aggregateName}' with identifier '{aggregateId}' was not found.")
+        {
+            AggregateName = aggregateName;
+            AggregateId = aggregateId;
+        }
+    }
+}
--- src/EnjoyCQRS/EventSource/Storage/Session.cs
+++ src/EnjoyCQRS/EventSource/Storage/Session.cs
@@ -2,28 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using EnjoyCQRS.Events;
+using EnjoyCQRS.EventSource.Exceptions;
 
 namespace EnjoyCQRS.EventSource.Storage
 {
     public class Session : ISession
     {
         private readonly IAggregateTracker _aggregateTracker;
-        private readonly IEventStore _domainEventStore;
+        private readonly IEventStore _eventStore;
         private readonly IEventPublisher _eventPublisher;
         private readonly List<Aggregate> _aggregates = new List<Aggregate>();
 
-        public Session(IAggregateTracker aggregateTracker, IEventStore domainEventStore, IEventPublisher eventPublisher)
+        public Session(IAggregateTracker aggregateTracker, IEventStore eventStore, IEventPublisher eventPublisher)
         {
             _aggregateTracker = aggregateTracker;
-            _domainEventStore = domainEventStore;
+            _eventStore = eventStore;
             _eventPublisher = eventPublisher;
         }
 
+        /// <summary>
+        /// Retrieves an aggregate, load your historical events and add to tracking.
+        /// </summary>
+        /// <typeparam name="TAggregate"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="AggregateNotFoundException"></exception>
         public TAggregate GetById<TAggregate>(Guid id) where TAggregate : Aggregate, new()
         {
-            var aggregate = new TAggregate();
+            var events = _eventStore.GetAllEvents(id);
 
-            var events = _domainEventStore.GetAllEvents(id);
+            if (events == null) throw new AggregateNotFoundException(typeof(TAggregate).Name, id);
+
+            var aggregate = new TAggregate();
 
             aggregate.LoadFromHistory(events);
 
@@ -32,20 +42,28 @@ namespace EnjoyCQRS.EventSource.Storage
             return aggregate;
         }
 
+        /// <summary>
+        /// Add the aggregate to tracking.
+        /// </summary>
+        /// <typeparam name="TAggregate"></typeparam>
+        /// <param name="aggregate"></param>
         public void Add<TAggregate>(TAggregate aggregate) where TAggregate : Aggregate
         {
             RegisterForTracking(aggregate);
         }
 
+        /// <summary>
+        /// Call <see cref="IEventStore.Save"/> in <see cref="IEventStore"/> passing aggregates.
+        /// </summary>
         public void Commit()
         {
-            _domainEventStore.BeginTransaction();
+            _eventStore.BeginTransaction();
 
             foreach (var aggregate in _aggregates)
             {
                 var changes = aggregate.UncommitedEvents.ToList();
                 
-                _domainEventStore.Save(changes);
+                _eventStore.Save(changes);
 
                 _eventPublisher.Publish<IDomainEvent>(changes);
 
@@ -55,14 +73,17 @@ namespace EnjoyCQRS.EventSource.Storage
             _aggregates.Clear();
 
             _eventPublisher.Commit();
-            _domainEventStore.Commit();
+            _eventStore.Commit();
         }
 
+        /// <summary>
+        /// Rollback <see cref="IEventPublisher"/>, <see cref="IEventStore"/> and remove aggregate tracking.
+        /// </summary>
         public void Rollback()
         {
             _eventPublisher.Rollback();
 
-            _domainEventStore.Rollback();
+            _eventStore.Rollback();
 
             foreach (var aggregate in _aggregates)
             {
