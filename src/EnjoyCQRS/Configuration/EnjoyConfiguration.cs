d2dfa89a5f59d9ef23f3e529313e24158833cf29
Nelson Junior <ircnelson@gmail.com>
2016-05-10 13:35:57 -0400

    fix scope resolver

--- src/EnjoyCQRS/Configuration/EnjoyConfiguration.cs
+++ src/EnjoyCQRS/Configuration/EnjoyConfiguration.cs
@@ -6,29 +6,27 @@ namespace EnjoyCQRS.Configuration
 {
     public class EnjoyConfiguration
     {
-        private readonly IScopeResolver _scopeResolver;
+        private readonly IResolver _resolver;
         private readonly HandlerDictionary _handlerDictionary;
         private readonly IEnjoyTypeScanner _scanner;
 
         public EnjoyConfiguration(
-            IScopeResolver scopeScopeResolver, 
+            IScopeResolver resolver, 
             HandlerDictionary handlerDictionary, 
             IEnjoyTypeScanner scanner)
         {
-            _scopeResolver = scopeScopeResolver;
+            _resolver = resolver;
             _handlerDictionary = handlerDictionary;
             _scanner = scanner;
         }
 
         public void Setup()
         {
-            var resolver = _scopeResolver.BeginScope();
-            
             var commands = HandlerHelper.Get<ICommand>(_scanner);
-            CommandWrapper.Wrap(resolver, _handlerDictionary, commands);
+            CommandWrapper.Wrap(_resolver, _handlerDictionary, commands);
 
             var events = HandlerHelper.Get<IDomainEvent>(_scanner);
-            EventWrapper.Wrap(resolver, _handlerDictionary, events);
+            EventWrapper.Wrap(_resolver, _handlerDictionary, events);
         }
     }
 }
