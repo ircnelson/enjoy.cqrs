7baca05578891fb83dab5e09149c53be8e72794f
Nelson Junior <ircnelson@gmail.com>
2016-06-01 10:32:38 -0400

    added Value Object abstraction

---
+++ src/EnjoyCQRS/ValueObjects/ValueObject.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace EnjoyCQRS.ValueObjects
+{
+    public abstract class ValueObject<TValueObject> : IEquatable<TValueObject> where TValueObject : ValueObject<TValueObject>
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            TValueObject other = obj as TValueObject;
+            
+            return Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            const int startValue = 17;
+            const int multiplier = 59;
+
+            var fields = GetFields();
+            var hashCode = startValue;
+
+            foreach (FieldInfo field in fields)
+            {
+                var value = field.GetValue(this);
+
+                if (value != null)
+                    hashCode = hashCode * multiplier + value.GetHashCode();
+            }
+
+            return hashCode;
+        }
+
+        public virtual bool Equals(TValueObject other)
+        {
+            if (other == null)
+                return false;
+
+            var t = GetType();
+            var otherType = other.GetType();
+            
+            if (t != otherType)
+                return false;
+
+            var fields = t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (FieldInfo field in fields)
+            {
+                object value1 = field.GetValue(other);
+                object value2 = field.GetValue(this);
+
+                if (value1 == null)
+                {
+                    if (value2 != null)
+                        return false;
+                }
+
+                else if (!value1.Equals(value2))
+                    return false;
+            }
+            
+            return true;
+        }
+        
+        private IEnumerable<FieldInfo> GetFields()
+        {
+            Type t = GetType();
+            List<FieldInfo> fields = new List<FieldInfo>();
+
+            while (t != typeof(object))
+            {
+                fields.AddRange(t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public));
+                t = t.BaseType;
+            }
+            
+            return fields;
+        }
+
+        public static bool operator ==(ValueObject<TValueObject> x, ValueObject<TValueObject> y)
+        {
+            if (ReferenceEquals(x, null))
+            {
+                return ReferenceEquals(y, null);
+            }
+
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(ValueObject<TValueObject> x, ValueObject<TValueObject> y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ test/EnjoyCQRS.UnitTests/ValueObjects/ValueObjectTests.cs
@@ -0,0 +1,100 @@
+﻿using EnjoyCQRS.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace EnjoyCQRS.UnitTests.ValueObjects
+{
+    public class ValueObjectTests
+    {
+        private const string CategoryName = "Unit";
+        private const string CategoryValue = "ValueObjects";
+
+        [Trait(CategoryName, CategoryValue)]
+        [Fact]
+        public void Should_be_identical()
+        {
+            FullName fullName1 = new FullName("Walter", "White");
+            FullName fullName2 = new FullName("Walter", "White");
+
+            fullName1.Should().Be(fullName2);
+        }
+
+        [Trait(CategoryName, CategoryValue)]
+        [Fact]
+        public void Should_be_equals_by_comparison()
+        {
+            FullName fullName1 = new FullName("Walter", "White");
+            FullName fullName2 = new FullName("Walter", "White");
+
+            (fullName1 == fullName2).Should().BeTrue();
+        }
+
+        [Trait(CategoryName, CategoryValue)]
+        [Fact]
+        public void Should_compare_two_instances_Given_that_right_operand_is_nullable()
+        {
+            FullName fullName1 = null;
+            FullName fullName2 = new FullName("Walter", "White");
+
+            (fullName2 == fullName1).Should().BeFalse();
+        }
+
+        [Trait(CategoryName, CategoryValue)]
+        [Fact]
+        public void Should_compare_two_instances_Given_that_left_operand_is_nullable()
+        {
+            FullName fullName1 = new FullName("Walter", "White"); 
+            FullName fullName2 = null;
+
+            (fullName2 == fullName1).Should().BeFalse();
+        }
+
+        [Trait(CategoryName, CategoryValue)]
+        [Fact]
+        public void Should_be_the_same_HashCode()
+        {
+            FullName fullName1 = new FullName("Walter", "White");
+            FullName fullName2 = new FullName("Walter", "White");
+
+            (fullName1.GetHashCode() == fullName2.GetHashCode()).Should().BeTrue();
+        }
+
+        [Trait(CategoryName, CategoryValue)]
+        [Theory]
+        [InlineData("Walter", "Walter2")]
+        [InlineData("Walter", null)]
+        public void Should_not_be_identical(string firstName1, string firstName2)
+        {
+            FullName fullName1 = new FullName(firstName1, "White");
+
+            FullName fullName2 = new FullName(firstName2, "White");
+
+            fullName1.Should().NotBe(fullName2);
+        }
+
+        [Trait(CategoryName, CategoryValue)]
+        [Theory]
+        [InlineData("Walter", "Walter2")]
+        [InlineData("Walter", null)]
+        public void Should_not_be_equals_by_comparison(string firstName1, string firstName2)
+        {
+            FullName fullName1 = new FullName(firstName1, "White");
+
+            FullName fullName2 = new FullName(firstName2, "White");
+
+            (fullName1 != fullName2).Should().BeTrue();
+        }
+    }
+
+    public class FullName : ValueObject<FullName>
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public FullName(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
