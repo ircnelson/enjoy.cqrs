fa21fdab012b91d259509e309645f48f6557cefa
Nelson Junior <ircnelson@gmail.com>
2016-07-11 08:27:23 -0400

    configures camel case json serialization

--- test/EnjoyCQRS.IntegrationTests.Shared/JsonTextSerializer.cs
+++ test/EnjoyCQRS.IntegrationTests.Shared/JsonTextSerializer.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using EnjoyCQRS.Core;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace EnjoyCQRS.IntegrationTests.Shared
 {
     public class JsonTextSerializer : ITextSerializer
     {
+        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
         public string Serialize(object @object)
         {
-            return JsonConvert.SerializeObject(@object);
+            return JsonConvert.SerializeObject(@object, _jsonSerializerSettings);
         }
 
         public object Deserialize(string textSerialized, string type)
@@ -18,7 +24,7 @@ namespace EnjoyCQRS.IntegrationTests.Shared
 
         public T Deserialize<T>(string textSerialized)
         {
-            return JsonConvert.DeserializeObject<T>(textSerialized);
+            return JsonConvert.DeserializeObject<T>(textSerialized, _jsonSerializerSettings);
         }
     }
 }
