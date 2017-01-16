b70e8c62283ba0693da7c14c98ca2d49bcf2ffe7
Nelson Junior <ircnelson@gmail.com>
2017-01-16 10:37:24 -0300

    Logger extensions

---
+++ src/EnjoyCQRS/Extensions/LoggerExtensions.cs
@@ -0,0 +1,60 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2016 Nelson Corrêa V. Júnior
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using EnjoyCQRS.Logger;
+using System;
+
+namespace EnjoyCQRS.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogInformation(this ILogger logger, string message)
+        {
+            logger.Log(LogLevel.Information, message);
+        }
+        
+        public static void LogDebug(this ILogger logger, string message)
+        {
+            logger.Log(LogLevel.Debug, message);
+        }
+
+        public static void LogError(this ILogger logger, string message, Exception exception = null)
+        {
+            logger.Log(LogLevel.Error, message, exception);
+        }
+
+        public static void LogWarning(this ILogger logger, string message)
+        {
+            logger.Log(LogLevel.Warning, message);
+        }
+
+        public static void LogTrace(this ILogger logger, string message)
+        {
+            logger.Log(LogLevel.Trace, message);
+        }
+
+        public static void LogCritical(this ILogger logger, string message)
+        {
+            logger.Log(LogLevel.Critical, message);
+        }
+    }
+}
